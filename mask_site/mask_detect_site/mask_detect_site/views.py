abd8df5a200f12a44ff3d000806679fdba8207b0
Patrick Xie <47607915+patrick-xie@users.noreply.github.com>
2021-02-07 01:26:18 -0500

    added ML model

--- mask_site/mask_detect_site/mask_detect_site/views.py
+++ mask_site/mask_detect_site/mask_detect_site/views.py
@@ -2,10 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect 
 from .forms import *
 
+import os
 import tensorflow as tf
 from tensorflow import keras
-from PIL import Image
+from keras.models import load_model
+import cv2
+import numpy as np
 
+model = load_model('model')
 # Create your views here. 
 def face_image_view(request): 
   
@@ -15,13 +19,21 @@ def face_image_view(request):
   
         if form.is_valid(): 
             form.save() 
-            return redirect('success') 
+            return redirect('success')
     else: 
         form = FaceForm() 
     return render(request, 'face_image_form.html', {'form' : form}) 
   
 def success(request):
-    image = Image.open('./media/images/upload.jpg')
+    image = cv2.imread('./media/images/upload.jpg')
+    image = cv2.resize(image, (128,128))
+    image = np.reshape(image, [1,128,128,3])
+    image = image/255.0
+    
+    prediction = model.predict(image)
+    prediction = prediction[0][0]
+    
     if(request.GET.get('mybtn')):
+        os.remove('./media/images/upload.jpg')
         return redirect('home')
-    return render(request, 'analysis.html')
+    return render(request, 'analysis.html', {'prediction' : prediction})
